242720502f2dde7a573c0b9a70f0c711612fe71f
Christine <chrsun@paypal.com>
2019-11-21 16:03:57 -0800

    add express server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const callBackendAPI = async () => {
+  const response = await fetch('/express_backend')
+  const body = await response.json()
+
+  if (response.status !== 200) {
+    throw Error(body.message) 
+  }
+  return body;
+}
+
 function App() {
+  callBackendAPI().then(res => console.log(res)).catch(err => console.log(err))
+
   return (
     <div className="App">
       <header className="App-header">
